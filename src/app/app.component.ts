6d6dd1438e8da9c22a473b216b6c41f2926e919b
diaba <hwar@cincinnatistate.edu>
2022-04-18 21:56:32 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { animate, group, state, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+import { buttonTextAnimation } from './animation';
+import { SocialCardType } from './constants/social-card-type';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    buttonTextAnimation('fbButtonTextAnimation', '80px'),
+    buttonTextAnimation('twButtonTextAnimation', '60px'),
+  ],
+})
+export class AppComponent {
+  title = 'ng-dynamic-components';
+  selectedCardType: SocialCardType;
+  cardTypes = SocialCardType;
+
+  setCardType(type: SocialCardType) {
+    this.selectedCardType = type;
+  }
+}
